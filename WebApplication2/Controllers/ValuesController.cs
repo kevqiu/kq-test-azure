40581fd9178c76bb4b8d62ef5fd2379f4951ba44
Kevin Qiu <kevinqiu96@gmail.com>
2018-09-23 22:47:46 -0700

    initial commit

---
+++ WebApplication2/Controllers/ValuesController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication2.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public ActionResult<string> Post([FromBody] string value)
+        {
+            return "POST world";
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public ActionResult<string> Put(int id, [FromBody] string value)
+        {
+            return "PUT world";
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public ActionResult<string> Delete(int id)
+        {
+            return "DELETE world";
+        }
+    }
+}
