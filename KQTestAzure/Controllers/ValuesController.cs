1830ee039a002d3771bd042e1d88db086d91f102
Kevin Qiu <kevinqiu96@gmail.com>
2018-09-23 23:53:48 -0700

    test

--- KQTestAzure/Controllers/ValuesController.cs
+++ KQTestAzure/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace WebApplication2.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2", "value3" };
+            return new string[] { "value1", "value2", "value5" };
         }
 
         // GET api/values/5
